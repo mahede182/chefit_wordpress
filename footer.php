c46520e725dfbbcacf8cc8532082bcf78f90dc12
mahede182 <53116487+mahede182@users.noreply.github.com>
2021-01-30 21:46:58 +0600

    all footer style and script are in here

---
+++ footer.php
@@ -0,0 +1,21 @@
+<!--////////////////////////////////////Footer-->
+<footer class="zerogrid">
+	<div class="wrap-footer">
+		<div class="row">
+			<?php get_sidebar(  );?>
+		</div>
+	</div>
+	<div class="copyright">
+		<div class="wrapper">
+			<?php echo '&copy;'.date('Y'). ' | ';?><a href="https://github.com/mahede182/chefIt"><?php echo get_bloginfo('name');?></a>
+			<ul class="quick-link f-right">
+				<li><a href="#">Privacy Policy</a></li>
+				<li><a href="#">Terms of Use</a></li>
+			</ul>
+		</div>
+	</div>
+</footer>
+	<!-- js -->
+</div>
+<?php wp_footer();?>
+</body></html>
