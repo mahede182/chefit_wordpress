66a95ae7911df8e937cb8509b596c7af474b93d3
mahede182 <53116487+mahede182@users.noreply.github.com>
2021-01-30 21:52:39 +0600

    reservation are escape the time.

---
+++ page-reservation.php
@@ -0,0 +1,31 @@
+<?php get_header();?>
+		<div id="main-content">
+			<div class="wrap-content">
+				<div class="row">
+					<div class="col-1-3">
+						<div class="wrap-col">
+							<h3>Complete the Submission Form</h3>
+							<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard</p><br/>
+							<h3>Or Just Make a Call</h3>
+							<p>+6221 888 888 90 <br>
+								+6221 888 88891</p>
+							<p>info@yourdomain.com</p>
+						</div>
+					</div>
+					<div class="col-2-3">
+						<div class="wrap-col">
+							<div class="contact">
+								<div id="contact_form">
+									<div id="contact">
+					<?php echo do_shortcode('[contact-form-7 id="43" title="chefit contact"]');?>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> 
+	</div>
+</section>
+<?php get_footer();?>
